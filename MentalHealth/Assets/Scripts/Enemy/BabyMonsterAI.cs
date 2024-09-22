b9b696741033db2482bddcaa85e7124358343bed
Rui Wang <wangnico@usc.edu>
2024-09-22 00:00:36 -0700

    small monster behavior script updated

--- MentalHealth/Assets/Scripts/Enemy/BabyMonsterAI.cs
+++ MentalHealth/Assets/Scripts/Enemy/BabyMonsterAI.cs
@@ -9,17 +9,29 @@ public class BabyMonsterAI : MonoBehaviour
     {
         Idle,
         Attack,
-        Retreat
+        Retreat,
+        Chase
     }
 
     MotionState m_state;
 
-    GameObject m_player;
+    Transform m_player;
     private Rigidbody2D m_rb;
 
     //  Idle State
-    public float m_detectRange = 10.0f;
     bool m_isActivated = false;
+
+    //  Attack state
+    public float detectionRange = 10f; // Range to detect the player
+    public float attackRange = 5f;     // Range to attack the player
+    public float retreatDistance = 0.5f; // Distance to retreat before attacking
+    public float speed = 40f;            // Speed of the fish
+    public float angleVariation = 45f;  // Variation in angles while attacking
+    public float retreatTime = 0.5f;
+    private float retreatTimer;
+
+    public float chasingSpeed = 50.0f;
+
 
     // shaking effect
     float m_shakingSpeed = 1.0f; //how fast it shakes
@@ -30,7 +42,7 @@ public class BabyMonsterAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_player = GameObject.FindGameObjectWithTag("Player");
+        m_player = GameObject.FindGameObjectWithTag("Player").transform;
         m_state = MotionState.Idle;
         m_rb = GetComponent<Rigidbody2D>();
     }
@@ -38,44 +50,137 @@ public class BabyMonsterAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.getState();
+        float distanceToPlayer = Vector2.Distance(transform.position, m_player.position);
 
+        this.getState(distanceToPlayer);
+
+         RotateToFacePlayer();
 
     }
 
-    public void getState()
+    public void getState(float distanceToPlayer)
     {
-        switch(m_state)
+        Debug.Log("Distance to Player: " + distanceToPlayer + " | Current State: " + m_state);
+
+        switch (m_state)
         {
             case MotionState.Idle:
-                CheckTarget();
+
+                if (distanceToPlayer <= detectionRange)
+                {
+                    m_state = MotionState.Attack;
+                }
+
                 break;
             
             case MotionState.Attack:
-                Attack();
+                if (distanceToPlayer <= attackRange)
+                {
+                    AttackPlayer();
+                }
+                else if (distanceToPlayer > detectionRange)
+                {
+                    m_state = MotionState.Idle;
+                }
                 break;
             
             case MotionState.Retreat:
+                RetreatFromPlayer();
+
+                break;
+
+            case MotionState.Chase:
+                Debug.Log("Chasing mode.");
+                if (distanceToPlayer >= attackRange + 0.1f  && distanceToPlayer <= detectionRange)
+                {
+                    ChasingPlayer();
+                }
+                else if (distanceToPlayer <= attackRange)
+                {
+                    m_state = MotionState.Attack;
+                }
+                else if (distanceToPlayer > detectionRange + 0.1f)
+                {
+                    m_state = MotionState.Idle;
+                }
+
                 break;
         }
+
     }
 
-    public void CheckTarget()
+    private void RotateToFacePlayer()
     {
-        if (!m_isActivated)
+        Vector2 directionToPlayer = m_player.position - transform.position;
+        float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg - 90;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    private void AttackPlayer()
+    {
+        Vector2 directionToPlayer = (m_player.position - transform.position).normalized;
+        Vector2 newPosition = (Vector2)transform.position + directionToPlayer * speed * Time.deltaTime;
+
+        m_rb.MovePosition(newPosition);
+
+        // If the enemy reaches close to the player, switch to retreat state
+        if (Vector2.Distance(transform.position, m_player.position) <= retreatDistance)
         {
-            if (Vector2.Distance(m_player.transform.position, transform.position) <= m_detectRange)
-            {
-                m_isActivated = true;
-                m_state = MotionState.Attack;
-            }
+            m_state = MotionState.Retreat;
+            retreatTimer = retreatTime;
+        }
+        else if (Vector2.Distance(transform.position, m_player.position) > attackRange &&
+                Vector2.Distance(transform.position, m_player.position) <= detectionRange)
+        {
+            m_state = MotionState.Chase;
+        }
+        else if (Vector2.Distance(transform.position, m_player.position) > detectionRange)
+        {
+            m_state = MotionState.Idle;
         }
     }
 
-    void Attack()
+    private void RetreatFromPlayer()
     {
-        Vector2 pos = transform.position;
-        pos.x = Mathf.Sin(Time.time * m_shakingSpeed) * m_shakingAmount;
-        
+        // Retreat a little bit
+        Vector2 directionToPlayer = (transform.position - m_player.position).normalized;
+        Vector2 retreatPosition = (Vector2)transform.position + directionToPlayer * retreatDistance;
+
+        m_rb.MovePosition(Vector2.MoveTowards(transform.position, retreatPosition, speed * Time.deltaTime));
+
+        // Countdown the retreat timer
+        retreatTimer -= Time.deltaTime;
+
+        // Once retreating is done, switch back to attacking if the player is still in range
+        if (retreatTimer <= 0)
+        {
+            if (Vector2.Distance(transform.position, m_player.position) <= attackRange)
+            {
+                m_state = MotionState.Attack;  
+            }
+            else if (Vector2.Distance(transform.position, m_player.position) > attackRange + 0.1f &&
+                Vector2.Distance(transform.position, m_player.position) <= detectionRange)
+            {
+                m_state = MotionState.Chase;
+            }
+            else
+            {
+                m_state = MotionState.Idle;
+            }
+             
+        }
     }
+
+    private void ChasingPlayer()
+    {
+        Debug.Log("Chasing Player.");
+        Vector2 directionToPlayer = (m_player.position - transform.position).normalized;
+        Vector2 newPosition = (Vector2)transform.position + directionToPlayer * chasingSpeed * Time.deltaTime;
+
+        m_rb.MovePosition(newPosition);
+
+
+    }
+
 }
